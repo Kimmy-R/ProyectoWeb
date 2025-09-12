64265b958adfc2b4e0cc62e50e1b799230fb9a9b
Kimmy-R <tatisrincon0726@gmail.com>
2025-09-11 19:38:57 -0500

    Papi update
    This is my firts push and pull

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+listaDatos = []
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+
+
+
+def guardar():  
+    datoObtenido = ""
+    if request.method == 'POST':
+        datoObtenido = request.form['in_nombres']
+        print(datoObtenido)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
